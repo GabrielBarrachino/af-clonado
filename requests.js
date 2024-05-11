7eaeb34efc2211c01782afffbd664ed308f2f646
GabrielBarrachino <gabrielbarrachino@outlook.com>
2024-05-11 13:27:39 -0300

    11/05-primeiras-adicoes

--- requests.js
+++ requests.js
@@ -1,15 +1,111 @@
 function sleep() {
-    const ms = Math.floor(Math.random() * (8000 - 1000 + 1) + 1000)
+    const ms = Math.floor(Math.random() * (8000 - 1000 + 1) + 1000);
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function getHistory() {
+async function getLogHistory() {
     const data = [
-        { text: "teste", link_param: "https://www.imovelguide.com.br/guidepro/corretor-de-imoveis-corretor-modelo/18111"},
-        { text: "teste2", link_param: "https://bitbucket.org/RavinNero/imovelguide_repo/src/e9858750f1d2/resources/views/painel/assistente/sold.blade.php"},
-        { text: "teste3", link_param: "https://bitbucket.org/RavinNero/imovelguide_repo/pull-requests/3242"},
+        { text: "Clique no link agoraaaaaaaaaaaa", link_param: "https://www.youtube.com"},
+    ];
+    await sleep();
+    return data;
+}
+
+function renderHistory(history) {
+    for (let index = 0; index < history.length; index += 1) {
+        const currentItem = history[index];
+        const text = document.createElement('a');
+        text.innerHTML = currentItem.text;
+        text.href = currentItem.link_param;
+        logContainer.appendChild(text);
+    }
+}
+
+window.onload = async () => {
+    const history = await getLogHistory();
+    renderHistory(history);
+};
+
+
+
+async function getTabs(){
+    const data = [
+        { 'id': 'xxx-yyy', 'text': 'Dashboard01', 'link_param': 'google'}
+    ]
+    sleep()
+    return data
+}
+ 
+async function getKeyMetrics(){
+    const data = [
+        { 'id': 'xxx-yyy', 'text': 'Dashboard01', 'bgcolor': 'blue', 'link_param': 'google', 'qty': 1}
     ]
     sleep()
     return data
 }
 
+async function getShortcuts(){
+    const data = [
+        { 'id': 'xxx-yyy', 'icon': 'mdi-xxx', 'text': 'Dashboard01', 'icon_color': 'red', 'bgcolor': 'blue', 'link_param': 'google', 'position': 1}
+    ]
+    sleep()
+    return data
+}
+
+async function getRecentFeaturesVisits(){
+    const data = [
+        { 'id': 'xxx-yyy', 'icon': 'mdi-xxx', 'text': 'Dashboard01', 'icon_color': 'red', 'bgcolor': 'blue', 'link_param': 'google', 'date': '10/01'}
+    ]
+    sleep()
+    return data
+}
+
+async function getMyBoards(){
+    const data = [
+        { 'id': 'xxx-yyy', 'icon': 'mdi-xxx', 'text': 'Dashboard01', 'icon_color': 'red', 'bgcolor': 'blue', 'link_param': 'google'}
+    ]
+    sleep()
+    return data
+}
+
+async function getSearchShortcuts(){
+    const data = [
+        { 'id': 'xxx-yyy', 'text': 'Dashboard01', 'itemchecked': true, 'disableToCheck': false, 'subitems': [
+            {
+                'id': 'xxx-yyy',
+                'text': 'Dashboard01',
+                'itemchecked': false,
+                'disableToCheck': false,
+                'subitems': [
+
+                ]
+            }
+        ]}
+    ]
+    sleep()
+    return data
+}
+
+async function getAddShortcut(){
+    const data = [
+        { 'id': 'xxx-yyy'}
+    ]
+    sleep()
+    return data
+}
+
+async function getRemoveShortcut(){
+    const data = [
+        { 'id': 'xxx-yyy'}
+    ]
+    sleep()
+    return data
+}
+ 
+async function getSavesShortcutSorting(){
+    const data = [
+        { 'id': 'xxx-yyy', 'position': 1}
+    ]
+    sleep()
+    return data
+}
