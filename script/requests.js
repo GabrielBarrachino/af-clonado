2f37b556f1d50ddce392f5c64195d4edcee846a6
Bernardo Costa <bfvcc01@gmail.com>
2024-05-13 21:20:41 -0300

    card redondol e hover feito

--- script/requests.js
+++ script/requests.js
@@ -3,6 +3,7 @@ function sleep() {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
 async function getLogHistory() {
     const data = [
         { text: "Contact New", link_param: "https://www.youtube.com"},
@@ -29,6 +30,9 @@ window.onload = async () => {
 
     const metric = await getKeyMetrics();
     renderKeyMetrics(metric);
+
+    const shortcuts = await getShortcuts()
+    renderShortcuts(shortcuts)
 };
 
 // Tabs (header)
@@ -43,7 +47,9 @@ async function getTabs(){
 // Indicadores
 async function getKeyMetrics(){
     const dataMetric = [
-        { 'id': 'xxx-yyy', 'text': 'Pendências de aprovação', 'bgcolor': '#FFF', 'link_param': 'google', 'qty': 1},
+        { 'id': 'xxx-yyy', 'text': 'Pendências de aprovação', 'bgcolor': '#FF5C00', 'link_param': 'google', 'qty': 10},
+        { 'id': 'xxx-yyy', 'text': 'Rascunho', 'bgcolor': '#0081FF', 'link_param': 'Github', 'qty': 0},
+
     ]
     sleep()
     return dataMetric
@@ -55,9 +61,15 @@ function renderKeyMetrics(metric){
         const linkMetric = document.createElement('a');
         linkMetric.id = currentItemMetric.id;
         linkMetric.href = currentItemMetric.link_param;
-        linkMetric.style.background = currentItemMetric.bgcolor;
+
+        const textNumberNotification = document.createElement('span')
+        textNumberNotification.classList.add('numberSpan')
+        textNumberNotification.innerHTML = currentItemMetric.qty
+        textNumberNotification.style.background = currentItemMetric.bgcolor;
+        linkMetric.appendChild(textNumberNotification)
 
         const textMetric = document.createElement('span')
+        textMetric.classList.add('textSpan')
         textMetric.innerHTML = currentItemMetric.text;
         linkMetric.appendChild(textMetric);
 
@@ -73,11 +85,51 @@ function renderKeyMetrics(metric){
 // Atalhos
 async function getShortcuts(){
     const data = [
-        { 'id': 'xxx-yyy', 'icon': 'mdi-xxx', 'text': 'Dashboard01', 'icon_color': 'red', 'bgcolor': 'blue', 'link_param': 'google', 'position': 1}
+        { 'id': 'xxx-yyy', 'icon': './assets/clients.svg', 'text': 'Clientes', 'icon_color': '#0281FF', 'bgcolor': '#E8F6FF', 'link_param': 'google', 'position': 1},
+        { 'id': 'xxx-yyy', 'icon': './assets/clientsGreen.svg', 'text': 'Solicitação de  mudanças GMUD e mais coisas e não foi ainda', 'icon_color': '#20CA41', 'bgcolor': '#E2F8E6', 'link_param': 'google', 'position': 1},
     ]
     sleep()
     return data
 }
+
+
+function renderShortcuts(short) {
+    for (let index = 0; index < short.length; index += 1) {
+        const currentItemShort = short[index];
+        const shortcut = document.createElement('a');
+        shortcut.classList.add('boxCont');
+        shortcut.id = currentItemShort.id;
+        shortcut.href = currentItemShort.link_param;
+
+        const divImgShort = document.createElement('div');
+        divImgShort.classList.add('boxImg');
+
+        // Criar o elemento span para o fundo colorido
+        const spanBg = document.createElement('span');
+        spanBg.style.backgroundColor = currentItemShort.bgcolor;
+        spanBg.classList.add('bgSpan');
+
+        // Criar o elemento para o ícone
+        const imgShort = document.createElement('img');
+        imgShort.classList.add('bgImgFundo');
+        imgShort.src = currentItemShort.icon;
+        imgShort.alt = currentItemShort.text;
+
+        divImgShort.appendChild(spanBg); // Adiciona o fundo colorido antes da imagem
+        divImgShort.appendChild(imgShort); // Adiciona a imagem
+
+        shortcut.appendChild(divImgShort);
+
+        const textShort = document.createElement('p');
+        textShort.innerHTML = currentItemShort.text;
+
+        shortcut.appendChild(textShort);
+
+        circulatesContainer.appendChild(shortcut);
+    }
+}
+
+
 
 async function getRecentFeaturesVisits(){
     const data = [
