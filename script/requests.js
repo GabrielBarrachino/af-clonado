2bbdf853b92ded6efec9a3dff7991dbf8d9cb66f
GabrielBarrachino <gabrielbarrachino@outlook.com>
2024-05-15 09:24:19 -0300

    tempo diminuido

--- script/requests.js
+++ script/requests.js
@@ -1,5 +1,5 @@
 function sleep() {
-    const ms = Math.floor(Math.random() * (8000 - 1000 + 1) + 1000);
+    const ms = Math.floor(Math.random() * (4000 - 1000 + 1) + 1000);
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
